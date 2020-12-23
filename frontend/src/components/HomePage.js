e241f7dae14fae36829b820dbff8e0cdf2500c85
rohan <rohanrajgautam@gmail.com>
2020-12-23 10:20:03 +0545

    fixed the homepage redirect and api call issue

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -15,38 +15,40 @@ import CreateRoomPage from './CreateRoomPage';
 //mui
 import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core';
 
-const renderHomePage = () => {
-  return (
-    <Grid container spacing={3}>
-      <Grid item xs={12} align='center'>
-        <Typography variant='h3' compact='h3'>
-          House Party
-        </Typography>
-      </Grid>
-      <Grid item xs={12} align='center'>
-        <ButtonGroup disableElevation variant='contained' color='primary'>
-          <Button color='primary' to='/join' component={Link}>
-            Join a Room
-          </Button>
-          <Button color='secondary' to='/create' component={Link}>
-            Create a Room
-          </Button>
-        </ButtonGroup>
-      </Grid>
-    </Grid>
-  );
-};
-
 const HomePage = () => {
-  const [roomCode, setRoomCode] = null;
+  const [roomCode, setRoomCode] = useState(null);
 
-  useEffect(async () => {
-    fetch('/api/user-in-room')
-      .then((response) => response.json())
-      .then((data) => {
-        setRoomCode: data.code;
-      });
-  });
+  useEffect(() => {
+    fetchRoom();
+  }, []);
+
+  const fetchRoom = async () => {
+    const response = await fetch('/api/user-in-room');
+    const data = await response.json();
+    setRoomCode(data.code);
+  };
+
+  const renderHomePage = () => {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align='center'>
+          <Typography variant='h3' compact='h3'>
+            AirMusic Cast
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align='center'>
+          <ButtonGroup disableElevation variant='contained'>
+            <Button color='primary' to='/join' component={Link}>
+              Join a Room
+            </Button>
+            <Button color='secondary' to='/create' component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  };
 
   return (
     <Router>
