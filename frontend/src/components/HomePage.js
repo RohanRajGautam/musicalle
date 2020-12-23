8ec1e6c59bafc5b65288cac8ee0d6af78817d470
rohan <rohanrajgautam@gmail.com>
2020-12-23 09:54:21 +0545

    worked on homepage in react

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,13 +12,56 @@ import Room from './Room';
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
 
+//mui
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core';
+
+const renderHomePage = () => {
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} align='center'>
+        <Typography variant='h3' compact='h3'>
+          House Party
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align='center'>
+        <ButtonGroup disableElevation variant='contained' color='primary'>
+          <Button color='primary' to='/join' component={Link}>
+            Join a Room
+          </Button>
+          <Button color='secondary' to='/create' component={Link}>
+            Create a Room
+          </Button>
+        </ButtonGroup>
+      </Grid>
+    </Grid>
+  );
+};
+
 const HomePage = () => {
+  const [roomCode, setRoomCode] = null;
+
+  useEffect(async () => {
+    fetch('/api/user-in-room')
+      .then((response) => response.json())
+      .then((data) => {
+        setRoomCode: data.code;
+      });
+  });
+
   return (
     <Router>
       <Switch>
-        <Route exact path='/'>
-          <p>This is the home page.</p>
-        </Route>
+        <Route
+          exact
+          path='/'
+          render={() => {
+            return roomCode ? (
+              <Redirect to={`/room/${roomCode}`} />
+            ) : (
+              renderHomePage()
+            );
+          }}
+        />
         <Route path='/join' component={RoomJoinPage} />
         <Route path='/create' component={CreateRoomPage} />
         <Route path='/room/:roomCode' component={Room} />
